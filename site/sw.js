0c52c96d5dec2ed628178d7072ec0aafbabc0b87
mlopes1970 <mlopes1970@gmail.com>
2026-08-14 15:35:17 -0300

    Update cache version from v3 to v4

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bracomil-share-v3';
+const CACHE = 'bracomil-share-v4';
 const APP_SHELL = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
